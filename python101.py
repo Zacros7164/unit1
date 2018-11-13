4f2fc03bfdeb92142bcb303a6cc066a7ed5b6edb
Zac Crosby <Zacros7164@gmail.com>
2018-11-13 12:05:41 -0500

    Firt commit of python101

---
+++ python101.py
@@ -0,0 +1,35 @@
+print "Hello, World";
+
+print """
+It was a dark and stormy night.
+    A murder happened.
+""";
+
+# comment wth python using a hashtag
+# highlight what you want and use command / to turn highlighted portion into a comment. most text editors will use this shortcut
+
+# variables
+# - strings, letters, numbers, or any other stuff
+# you can make with a keyboard
+
+# variables do not make a program faster.
+# they make the program slower.
+# variables make it easier for us to write a program.
+
+# This is how you declare a variable
+theBestClass = "the 11-18 immersive"
+print theBestClass
+
+# Data types
+# - Programming languages see different types of varibles differently
+# - string = English stuff.
+# - Number = something with a number or a - or e for standard notation.
+#     - floats = decimal point in the number
+#     - integers = whole number (no decimal point)
+# - Booleans = true or false, on or off, 1 or 0
+# - list = a bunch of things in one variable, most other languages call this an array
+# - Dictionaries = a variable of variables
+# - Objects - super dictionaries 
+
+# Primitvie Data types = String, Numbers, Booleans (like base elements)
+# Abstract data types = List, Dictionaries, Objects (things made up of primitive dtata types, like H2O OR NaCl)
