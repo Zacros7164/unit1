4d8ea87e5551870656e590f788d8d49afaf08418
Zac Crosby <Zacros7164@gmail.com>
2018-11-13 14:28:19 -0500

    add operators

--- python101.py
+++ python101.py
@@ -1,47 +1,81 @@
-print "Hello, World";
+# print "Hello, World";
 
-print """
-It was a dark and stormy night.
-    A murder happened.
-""";
+# print """
+# It was a dark and stormy night.
+#     A murder happened.
+# """;
 
-# comment wth python using a hashtag
-# highlight what you want and use command / to turn highlighted portion into a comment. most text editors will use this shortcut
+# # comment wth python using a hashtag
+# # highlight what you want and use command / to turn highlighted portion into a comment. most text editors will use this shortcut
 
-# variables
-# - strings, letters, numbers, or any other stuff
-# you can make with a keyboard
+# # variables
+# # - strings, letters, numbers, or any other stuff
+# # you can make with a keyboard
 
-# variables do not make a program faster.
-# they make the program slower.
-# variables make it easier for us to write a program.
+# # variables do not make a program faster.
+# # they make the program slower.
+# # variables make it easier for us to write a program.
 
-# This is how you declare a variable
-theBestClass = "the 11-18 immersive"
-print theBestClass
+# # This is how you declare a variable
+# theBestClass = "the 11-18 immersive"
+# print theBestClass
 
-# Data types
-# - Programming languages see different types of varibles differently
-# - string = English stuff.
-# - Number = something with a number or a - or e for standard notation.
-#     - floats = decimal point in the number
-#     - integers = whole number (no decimal point)
-# - Booleans = true or false, on or off, 1 or 0
-# - list = a bunch of things in one variable, most other languages call this an array
-# - Dictionaries = a variable of variables
-# - Objects - super dictionaries 
+# # Data types
+# # - Programming languages see different types of varibles differently
+# # - string = English stuff.
+# # - Number = something with a number or a - or e for standard notation.
+# #     - floats = decimal point in the number
+# #     - integers = whole number (no decimal point)
+# # - Booleans = true or false, on or off, 1 or 0
+# # - list = a bunch of things in one variable, most other languages call this an array
+# # - Dictionaries = a variable of variables
+# # - Objects - super dictionaries 
 
-# Primitvie Data types = String, Numbers, Booleans (like base elements hydrogen, oxygen, etc)
-# Abstract data types = List, Dictionaries, Objects (things made up of primitive dtata types, like H2O OR NaCl)
-month = "November"
-print type(month)
-date = 13
-print type(date)
-dateAsfloat = 13.0
-print type(dateAsfloat)
-aBool = True 
-print type(aBool)
-aList = []
-print type(aList)
-aDictionary = {}
-print type(aDictionary)
+# # Primitvie Data types = String, Numbers, Booleans (like base elements hydrogen, oxygen, etc)
+# # Abstract data types = List, Dictionaries, Objects (things made up of primitive dtata types, like H2O OR NaCl)
+# month = "November"
+# print type(month)
+# date = 13
+# print type(date)
+# dateAsfloat = 13.0
+# print type(dateAsfloat)
+# aBool = True 
+# print type(aBool)
+# aList = []
+# print type(aList)
+# aDictionary = {}
+# print type(aDictionary)
+
+# # concatenate is programming speak for add things together
+# first = "Zac"
+# last = "Crosby"
+# fullName = first + last
+# fullName = first + " " + last
+# print fullName
+
+# # fourteen = 10 + 4
+# # print fourteen
+
+# # cast = change a variable to a new data type
+# fourteen = int("10") + 4
+# print fourteen
+
+# Math = +, -, /, *, %
+# print 2+2
+# print 2-2
+# print 2/2
+# print 2*2
+# # % = modulus, Modulus divides the number and gives you the remainder
+# print 2%2 
+# print 2%3
+# print 2**3
+# just like concatenation the multiplication symbol can be used to replicate a string multiple times
+# a string and a * and a number = give me X strings 
+print "-" * 20
+
+# python does not have a simple incrementer
+num = 1;
+# num ++
+num += 1;
+
+
