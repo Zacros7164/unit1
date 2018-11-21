dfcd7473e19319522b128baa6bd3a19234da1b81
Zac Crosby <Zacros7164@gmail.com>
2018-11-21 16:05:50 -0500

    added arrows, and bounding box for hero

--- pygame_shooter/bad_guy.py
+++ pygame_shooter/bad_guy.py
@@ -1,11 +1,16 @@
 from math import hypot
+from pygame.sprite import Sprite
+import pygame 
 
-
-class BadGuy(object):
+class BadGuy(Sprite):
     def __init__(self):
+        super(BadGuy,self).__init__()
         self.x = 200
         self.y = 200
         self.speed = 4
+        self.rect = pygame.Rect(0,0,30,32)
+        self.rect.centerx = self.x
+        self.rect.centery = self.y
     def update_me(self, theHero):
         dx = self.x - theHero.x
         dy = self.x - theHero.y
@@ -14,3 +19,4 @@ class BadGuy(object):
         dy = dy/ dist
         self.x -= dx * self.speed
         self.y -=dy * self.speed
+        self.rect.x = self.x
--- pygame_shooter/game.py
+++ pygame_shooter/game.py
@@ -3,6 +3,10 @@
 import pygame
 from Hero import Hero
 from bad_guy import BadGuy
+from Arrow import Arrow
+# get Group and group collide from the sprite module
+from pygame.sprite import Group, groupcollide
+
 # 2. initialize pygame
 # Why do we need to do this? because they told us to
 pygame.init()
@@ -13,8 +17,16 @@ pygame_screen = pygame.display.set_mode(screen_size)
 # set the title of the window that opens...
 pygame.display.set_caption('Archer')
 
+# ths is our hero object
 theHero = Hero()
+# this is our bad guy object
 bad_guy = BadGuy()
+bad_guys = Group()
+bad_guys.add(bad_guy)
+# this is a list to hold our arrows (quiver)
+# arrows= []
+# a group is a special pygame "list" for Sprites
+arrows = Group()
 
 # ===========VARIABLES FOR OUR GAME==========
 background_image = pygame.image.load('background.png')
@@ -26,6 +38,10 @@ arrow_image = pygame.image.load('arrow.png')
 #     'x': 0,
 #     'y': 0
 # }
+
+bg_music = pygame.mixer.Sound('faf.wav')
+bg_music.play()
+
 # ==============MAIN GAME LOOP==============
 gameOn = True
 # the loop will run as long as our bool is True
@@ -56,6 +72,11 @@ while gameOn:
                 # the user pressed the down arrow, AND isn't off the screen! Move the guy down
                 # theHero.y += 10
                 theHero.should_move('down')
+            elif event.key == 32:
+                # space bar = fire!!
+                new_arrow = Arrow(theHero)
+                arrows.add(new_arrow)
+
         elif event.type == pygame.KEYUP:
             # the user released a key
             print event.key
@@ -75,8 +96,23 @@ while gameOn:
     # 2. where to draw it
     # in the docs.... SURFACE(their name) = our "pygame_screen"
     pygame_screen.blit(background_image,[0,0])
+    # Draw the hero
     theHero.draw_me()
-    bad_guy.update_me(theHero)
     pygame_screen.blit(hero_image, [theHero.x, theHero.y])
-    pygame_screen.blit(monster_image, [bad_guy.x, bad_guy.y])
+
+    # Draw the arrows
+    for arrow in arrows:
+        arrow.update_me()
+        pygame_screen.blit(arrow_image, [arrow.x, arrow.y])
+
+    # inside ()
+    # 1. group 1
+    # 2. group 2
+    # 3. if something in group 1 hits something in group 2, should i remove it from group 1? true = yes
+    # 4. if something in group 2 hits something in group 1, should I remove it from group 2? false = no
+    arrow_hit= groupcollide(arrows,bad_guys,True,True)
+    # move the bad guys
+    for bad_guy in bad_guys:
+        bad_guy.update_me(theHero)
+        pygame_screen.blit(monster_image, [bad_guy.x, bad_guy.y])
     pygame.display.flip()
